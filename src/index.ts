75c1ef499dad4291b47f8e633d7a6c8154445e4e
Jefferson <jeffersong@ciandt.com>
2022-02-08 00:19:43 -0300

    poc

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 import { setFailed, getInput, setOutput } from '@actions/core';
 import {context} from '@actions/github';
+import fetch from 'node-fetch';
+import { mainModule } from 'process';
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = getInput('full-uri');
-  console.log(`Hello ${nameToGreet}!`);
-  setOutput("notify", nameToGreet);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error: any) {
-  setFailed(error.message);
+async function main (){
+	try {
+		// `who-to-greet` input defined in action metadata file
+		const url = getInput('full-uri');
+		console.log(`Hello ${url}!`);
+		setOutput("notify", url);
+
+		const data = JSON.stringify({
+			'text': 'Hello from a Node script!',
+		});
+	
+		await fetch(url, {
+		method: 'post',
+		body: data,
+		headers: {'Content-Type': 'application/json'}
+	});
+	
+		// Get the JSON webhook payload for the event that triggered the workflow
+		const payload = JSON.stringify(context.payload, undefined, 2)
+		console.log(`The event payload: ${payload}`);
+	} catch (error: any) {
+		setFailed(error.message);
+	}
 }
+
+main()
