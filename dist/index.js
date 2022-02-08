ade11bdd1fbfc2a8ef41d388f2cbf564c8de6681
Jefferson <jeffersong@ciandt.com>
2022-02-07 23:01:11 -0300

    added request chat

--- dist/index.js
+++ dist/index.js
@@ -15532,7 +15532,7 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(5127);
 const github = __nccwpck_require__(3134);
-const { fetch } = __nccwpck_require__(4861);
+const fetch = __nccwpck_require__(4861);
 
 main();
 
