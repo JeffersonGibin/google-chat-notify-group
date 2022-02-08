79678c4370f4f583071ff653489fb843b1f57bbd
Jefferson <jeffersong@ciandt.com>
2022-02-08 00:12:36 -0300

    poc

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import core from '@actions/core';
-import github from '@actions/github';
+import { setFailed, getInput, setOutput} from '@actions/core';
+import {context} from '@actions/github';
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('full-uri');
+  const nameToGreet = getInput('full-uri');
   console.log(`Hello ${nameToGreet}!`);
-  core.setOutput("notify", nameToGreet);
+  setOutput("notify", nameToGreet);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error: any) {
-  core.setFailed(error.message);
+  setFailed(error.message);
 }
