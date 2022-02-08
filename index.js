d45bc828ee3e9617343ae391b855feb685193f56
Jefferson <jeffersong@ciandt.com>
2022-02-07 23:42:45 -0300

    added typescript

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const fetch = require('node-fetch');
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = __importDefault(require("@actions/core"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
 main();
-
-async function main() {
-	try {
-		const fullUri = core.getInput("full-uri");
-		const time = new Date().toTimeString();
-		const output = `[${time}] A notify was sent to group!`;
-
-		core.setOutput("notify", output);
-		const payload = JSON.stringify(github.context.payload, undefined, 2);
-
-		const body = JSON.stringify({
-			text: "Hello Group!",
-		});
-
-		await fetch(fullUri, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json; charset=UTF-8",
-			},
-			body,
-		});
-
-		console.log(`The event payload: ${payload}`);
-	} catch (error) {
-		core.setFailed(error.message);
-	}
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const fullUri = core_1.default.getInput("full-uri");
+            const body = JSON.stringify({
+                text: "Hello Group!",
+            });
+            yield (0, node_fetch_1.default)(fullUri, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json; charset=UTF-8",
+                },
+                body,
+            });
+        }
+        catch (error) {
+            //core.setFailed(error.message);
+        }
+    });
 }
